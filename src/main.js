dcaa0dbb4b53bfe2b3b5a47125532c73a4936cf1
Miguel Jimenez <miguels.jimenezv@gmail.com>
2025-01-03 19:17:32 -0300

    Correciòn de errores

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 /* import './assets/main.css' */
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-
 
 const app = createApp(App);
 
